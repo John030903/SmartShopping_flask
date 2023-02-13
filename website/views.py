32b4e0ecc0c5f4bb478f161560634f715f6d35be
John030903 <nguyenhoangkhanhduy030903@gmail.com>
2023-02-13 15:10:04 +0700

    delete search key

--- website/views.py
+++ website/views.py
@@ -5,9 +5,9 @@ views = Blueprint('view', __name__)
 
 @views.route('/', methods=['GET','POST'])
 def home():
-    if request.method == "POST":
-        search_key = request.form.get('search_key')
-        items = load_data(search_key)
-        return render_template('items.html', items=items, title=search_key)
-    else:
-        return render_template('base.html')
+    # if request.method == "POST":
+    #     search_key = request.form.get('search_key')
+    items = load_data("Hoa giả")
+    return render_template('items.html', items=items, title="Hoa giả")
+    # else:
+    #     return render_template('base.html')
